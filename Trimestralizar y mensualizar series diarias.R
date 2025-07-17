4cebf557880f2db17c4ba19f603ce454519578ea
ianalejandrosanchez <ianalejandrocamachosanchez@gmail.com>
2025-07-16 22:05:57 -0600

    Add files via upload

---
+++ Trimestralizar y mensualizar series diarias.R
@@ -0,0 +1,37 @@
+# Mas información visitar 
+# https://ianalejandrosanchez.github.io/website/
+
+# Estructura requerida:
+# +----------------------+
+# |Fecha     |  valor    |
+# +----------+-----------+
+# |dd/mm/aaaa| 0.00000   |
+# |     .    |    .      |
+# |     .    |    .      |
+# |----------------------|
+
+# Usar install.packages() si se requiere instalar alguna paquetería.
+
+library(readxl)
+library(dplyr)
+library(lubridate)
+
+mi_base <- read_excel("")
+
+# Trimestralizar datos diaros
+datos_trimestrales <- mi_base %>%
+  mutate(trimestre = paste0(year(Fecha), "-Q", quarter(Fecha))) %>%
+  group_by(trimestre) %>%                                         
+  summarize(valor_trimestral = mean(valor, na.rm = TRUE))         
+print(datos_trimestrales)
+
+# Mensualizar datos diarios
+datos_mensuales <- mi_base %>%
+   mutate(mes = paste0(year(Fecha), "-M", month(Fecha))) %>%
+   group_by(mes) %>%                                         
+   summarize(valor_mensual = mean(valor, na.rm = TRUE))         
+print(datos_mensuales)
+
+# Exportar series a .csv
+write.csv(datos_mensuales, "datos_mensuales.csv", row.names = FALSE)
+write.csv(datos_trimestrales, "datos_trimestrales.csv", row.names = FALSE)
